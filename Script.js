8b8fa911c69f5ad0d4b9eb5c3ba92272b99e279a
Sparebola <classic74rus@gmail.com>
2021-05-10 23:53:58 +0500

    Операторы: сложение, вычитание, деление и умножение Логические операторы Преобразование типов Циклы и ветвление кода, конструкция “switch”

--- Script.js
+++ Script.js
@@ -8,6 +8,164 @@ testTypeData();
 // Комментарии
 testComments();
 
+// Операторы: сложение, вычитание, деление и умножение
+testOperators();
+
+// Логические операторы
+testBoolean();
+
+// Преобразование типов
+testType();
+
+// Циклы и ветвление кода, конструкция “switch”
+testWhile();
+
+function testWhile(params) {
+    // Цикл.
+    while (true) {
+        break // Выходим из беск-цикла
+    }
+
+    // Цикл пройдёт 1 раз, ведь условие он проверяет после выполнения тела.
+    let bool = false;
+    do {
+        console.log("Итерация")
+    } while (bool);
+
+    //     начало; условие; шаг
+    for (let i = 0; i < 3; i++) { // выведет 0, затем 1, затем 2
+        console.log(i);
+    }
+
+    // continue
+    for (let i = 0; i < 10; i++) {
+
+        // если true, пропустить оставшуюся часть тела цикла
+        if (i % 2 == 0) continue;
+      
+        console.log(i); // 1, затем 3, 5, 7, 9
+    }
+
+    // Метки. С вложенного цикла сразу остановить 2 цикла.
+    outer: for (let i = 0; i < 3; i++) {
+
+        for (let j = 0; j < 3; j++) {
+
+            break outer; // Вход
+        }
+    }
+    // Выход
+
+    // Применение условий
+    const year = 2014;
+    if (year < 2015) {
+        console.log( 'Это слишком рано...' );
+    } else if (year > 2015) {
+        console.log( 'Это поздновато' );
+    } else {
+        console.log( 'Верно!' );
+    }
+
+    // Тернарный оператор
+    const age = 10;
+    let accessAllowed = (age > 18) ? "Да" : "Нет";
+    console.log(accessAllowed); // false
+
+    // Конструкция "switch"
+    const x = "value1";
+    switch(x) {
+        case 'value1':  // if (x === 'value1')
+            console.log("Done!") // Сработает
+            break // не обязательно. Если не поставлен, весь ниже код выполнится, минуя проверки
+
+        case 'value2':  // if (x === 'value2')
+            // Код
+            break
+
+        default:
+            // Код
+            break
+    }
+
+    // Группировка «case». Теперь оба варианта 3 и 5 выводят одно сообщение.
+    const s = 5;
+    switch (s) {
+        case 4:
+            console.log('Правильно!');
+            break;
+      
+        case 3: // (*) группируем оба case
+        case 5:
+            console.log('Неправильно!');
+            console.log("Может вам посетить урок математики?");
+            break;
+      
+        default:
+            console.log('Результат выглядит странновато. Честно.');
+    }
+}
+
+function testType(params) {
+    let value = true;
+    value = String(value); // теперь value это строка "true"
+
+    value = "123";
+    value = Number(value); // становится числом 123
+}
+
+function testBoolean(params) {
+    let test = ( 1 || 0 ); // 1 - вернуло первое истинное значение
+    test = ( null || 0 || 1 ); // 1 (первое истинное значение)
+    test = ( undefined || null || 0 ); // 0 (поскольку все ложно, возвращается последнее значение)
+
+    test = ( 1 && 2 && null && 3 ); // null (первое ложное значание)
+    test = ( 1 && 2 && 3 ); // 3 (Все верны, последнее верное значение)
+
+    test = ( !true ); // false
+    test = ( !0 ); // true
+    // Аналог Boolean()
+    test = ( !!"non-empty string" ); // true
+    test = ( !!null ); // false
+}
+
+function testOperators(params) {
+    // Объединяем строки
+    let s = "моя" + "строка";
+
+    // Ответ будет в виде строки: 12
+    s = "1" + 2;
+
+    // Ответ будет 33. 1 + 2 = 3, дальше идёт сложение со строкой
+    s = 1 + 2 + "3";
+
+    // Другие арифметические функции работают как обычно
+    s = 6 - '2'; // 4. "Два" приводятся к числу
+    s = "6" / "2"; // 3. Оба операнда приводятся к числам
+
+    // Сокращение Number()
+    s = +true // 1
+    s = Number(true) // 1
+
+    // Сокращение операций
+    let n = 2;
+    n += 5; // теперь n = 7 (работает как n = n + 5)
+    n *= 2; // теперь n = 14 (работает как n = n * 2)
+    // И тд.
+
+    let counter = 2;
+    counter++; // 3. Работает как counter = counter + 1, просто запись короче
+    counter--; // 1
+
+    // Префиксная форма ++counter увеличивает counter и возвращает новое значение 2
+    counter = 1;
+    let a = ++counter; // 2
+
+    // Постфиксная форма counter++ также увеличивает counter, но возвращает старое значение (которое было до увеличения). Так что покажет 1.
+    a = counter++; // 1
+
+    a = "  1  " - 1 // 0. Пробелы обрезаются
+}
+
 function testComments(params) {
     // Однострочные комментарии
     /*
